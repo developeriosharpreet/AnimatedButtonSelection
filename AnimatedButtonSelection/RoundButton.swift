fc497862421def749d4a1acf3b515bce4284d268
Harpreet Singh <developeriosharpreet@gmail.com>
2018-06-10 16:28:28 +0530

    Added Xib file

---
+++ AnimatedButtonSelection/RoundButton.swift
@@ -0,0 +1,21 @@
+//
+//  RoundButton.swift
+//  AnimatedButtonSelection
+//
+//  Created by Harpreet on 10/06/18.
+//  Copyright © 2018 Animators. All rights reserved.
+//
+
+import UIKit
+
+class RoundButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
